9203118243be287795d6061709148b3e904c17fe
Davina Dou <doud@berkeley.edu>
2021-03-17 00:26:36 -0700

    need to fix image path

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/components/md-store`,
+        path: `${__dirname}/src/md-store`,
       },
     },
     `gatsby-transformer-remark`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,30 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { renderToString } from 'react-dom/server'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-// import About from "../md-store/assets/about.json"
+import About from "../md-store/assets/about.json"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>{About.name}</h1>
+    <p>{About.year}</p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src={"../../static/img/crying-cat.jpg"}
       width={300}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
   </Layout>
 )
+
+//src={"../../static" + renderToString(About.image)}
+//src={"../../static/img/crying-cat.jpg"}
+//src={"../../static" + About.image}
 
 export default IndexPage
